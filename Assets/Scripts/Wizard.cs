90d34af40ff0445eb75a2f2bea1982c2729d012f
bozar <5583771+Bozar@users.noreply.github.com>
2019-05-31 13:14:54 +0800

    Fill the screen with mock-up text.

--- Assets/Scripts/Wizard.cs
+++ Assets/Scripts/Wizard.cs
@@ -9,6 +9,19 @@ namespace AxeMan.GameSystem
             Debug.Log(GameCore.AxeManCore.GetComponent<GameCore>().Hello);
             //Debug.Log(GetComponent<GameCore>().Hello);
             //Debug.Log(FindObjectOfType<GameCore>().Hello);
+
+            GameObject dummy;
+            for (float i = -6; i < -1.5f; i += 0.5f)
+            {
+                for (float j = -4; j < 0.5f; j += 0.5f)
+                {
+                    if ((j == 0) || (i == -2))
+                    {
+                        dummy = Instantiate(Resources.Load("Dummy") as GameObject);
+                        dummy.transform.localPosition = new Vector3(i, j);
+                    }
+                }
+            }
         }
     }
 }
