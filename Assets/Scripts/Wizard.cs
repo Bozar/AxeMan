b68451743e0a6925aa55fc907fc3bf1006acbb13
bozar <5583771+Bozar@users.noreply.github.com>
2019-05-31 17:08:51 +0800

    Adjust UI layout: Merge Modeline with World.

--- Assets/Scripts/Wizard.cs
+++ Assets/Scripts/Wizard.cs
@@ -11,11 +11,26 @@ namespace AxeMan.GameSystem
             //Debug.Log(FindObjectOfType<GameCore>().Hello);
 
             GameObject dummy;
-            for (float i = -6; i < -1.5f; i += 0.5f)
+            //for (float i = -6; i < -1.5f; i += 0.5f)
+            //{
+            //    for (float j = -4; j < 0.5f; j += 0.5f)
+            //    {
+            //        if ((j == 0) || (i == -2))
+            //        {
+            //            dummy = Instantiate(Resources.Load("Dummy") as GameObject);
+            //            dummy.transform.localPosition = new Vector3(i, j);
+            //        }
+            //    }
+            //}
+            float minX = -5.5f;
+            float maxX = -1.5f;
+            float minY = -3.5f;
+            float maxY = 0.5f;
+            for (float i = minX; i <= maxX; i += 0.5f)
             {
-                for (float j = -4; j < 0.5f; j += 0.5f)
+                for (float j = minY; j <= maxY; j += 0.5f)
                 {
-                    if ((j == 0) || (i == -2))
+                    if ((j == maxY) || (i == maxX))
                     {
                         dummy = Instantiate(Resources.Load("Dummy") as GameObject);
                         dummy.transform.localPosition = new Vector3(i, j);
