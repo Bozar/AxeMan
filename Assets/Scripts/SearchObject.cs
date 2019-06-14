00a330ef7bed40b77cf224a5b3e420c22cf1c354
bozar <5583771+Bozar@users.noreply.github.com>
2019-06-14 17:41:16 +0800

    Add a blank class: SearchObject.

---
+++ Assets/Scripts/SearchObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace AxeMan.GameSystem
+{
+    public interface ISearchObject
+    {
+        GameObject[] Search(int x, int y);
+
+        GameObject[] Search(MainTag mTag);
+
+        GameObject[] Search(SubTag sTag);
+    }
+
+    public class SearchObject : MonoBehaviour, ISearchObject
+    {
+        public GameObject[] Search(int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GameObject[] Search(MainTag mTag)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GameObject[] Search(SubTag sTag)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
