f1c9793b468f24b7b4bb7fa49e4a36f5d0f44dd0
bozar <5583771+Bozar@users.noreply.github.com>
2019-06-14 10:00:30 +0800

    Merge DungeonBoardCore into DungeonBoard.

--- Assets/Scripts/DungeonBoard.cs
+++ Assets/Scripts/DungeonBoard.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 
 namespace AxeMan.GameSystem
 {
+    public interface IDungeonBoard
+    {
+        bool AddObject(int x, int y, IDungeonObject ido, bool overwrite);
+
+        bool ExistObject(int x, int y, MainTag mtag);
+
+        IDungeonObject GetObject(int x, int y, MainTag mtag);
+
+        IDungeonObject RemoveObject(int x, int y, MainTag mtag);
+    }
+
+    public interface IDungeonObject
+    {
+        GameObject Data { get; }
+
+        MainTag MTag { get; }
+    }
+
     // DungeonBoard is a data hub which records and updates every dungeon
     // object's position.
     public class DungeonBoard : MonoBehaviour, IDungeonBoard
@@ -100,5 +118,18 @@ namespace AxeMan.GameSystem
             GetComponent<CreateObject>().CreatedObject
                 += DungeonBoard_CreatedObject;
         }
+    }
+
+    public class DungeonObject : IDungeonObject
+    {
+        public DungeonObject(MainTag mTag, GameObject data)
+        {
+            MTag = mTag;
+            Data = data;
+        }
+
+        public GameObject Data { get; }
+
+        public MainTag MTag { get; }
     }
 }
