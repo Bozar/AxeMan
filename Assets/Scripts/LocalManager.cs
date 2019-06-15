f460db68bd5fe92a8ee5a0babca30633b8ee44be
bozar42 <5583771+Bozar@users.noreply.github.com>
2019-06-15 12:28:52 +0800

    Add GetPosition() to LocalManager.

--- Assets/Scripts/LocalManager.cs
+++ Assets/Scripts/LocalManager.cs
@@ -6,11 +6,19 @@ namespace AxeMan.DungeonObject
 {
     public interface ILocalManager
     {
+        int[] GetPosition();
+
         void Remove();
     }
 
     public class LocalManager : MonoBehaviour, ILocalManager
     {
+        public int[] GetPosition()
+        {
+            return GameCore.AxeManCore.GetComponent<ConvertCoordinate>()
+                .Convert(transform.position);
+        }
+
         public void Remove()
         {
             GameCore.AxeManCore.GetComponent<RemoveObject>().Remove(gameObject);
