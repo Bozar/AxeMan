629cbd14f577fd38f25f05f0123fb33ad8d7b2a1
bozar <5583771+Bozar@users.noreply.github.com>
2019-06-20 17:15:46 +0800

    Add PCInput.cs.

--- Assets/Scripts/ObjectTag.cs
+++ Assets/Scripts/ObjectTag.cs
@@ -4,6 +4,8 @@ namespace AxeMan.GameSystem
 {
     public enum BlueprintTag { INVALID, Altar, Floor, Trap, Actor, }
 
+    public enum CommandTag { INVALID, Test, }
+
     public enum MainTag { INVALID, Building, Trap, Actor, Floor, }
 
     public enum SearchEventTag { Position, MainTag, SubTag, }
---
+++ Assets/Scripts/PCInput.cs
@@ -0,0 +1,28 @@
+﻿using AxeMan.GameSystem;
+using UnityEngine;
+
+namespace AxeMan.DungeonObject.InputManager
+{
+    public interface IInputManager
+    {
+        CommandTag ConvertInput();
+    }
+
+    public class PCInput : MonoBehaviour, IInputManager
+    {
+        public CommandTag ConvertInput()
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                Debug.Log("Input j");
+                return CommandTag.Test;
+            }
+            return CommandTag.INVALID;
+        }
+
+        private void Update()
+        {
+            ConvertInput();
+        }
+    }
+}
