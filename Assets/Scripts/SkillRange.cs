060e84eb6902d212af50923b9ccb5a75f849f525
bozar <5583771+Bozar@users.noreply.github.com>
2019-07-23 11:45:25 +0800

    Check distance when trying to use a skill.

--- Assets/Scripts/SkillRange.cs
+++ Assets/Scripts/SkillRange.cs
@@ -1,4 +1,7 @@
-﻿using AxeMan.GameSystem.GameDataTag;
+﻿using AxeMan.GameSystem;
+using AxeMan.GameSystem.GameDataTag;
+using AxeMan.GameSystem.GameMode;
+using AxeMan.GameSystem.SearchGameObject;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -29,8 +32,25 @@ namespace AxeMan.DungeonObject.ActorSkill
             skillRangeDict = new Dictionary<SkillNameTag, int>();
         }
 
+        private void SkillRange_VerifyingSkill(object sender,
+            VerifyingSkillEventArgs e)
+        {
+            int[] aimMarker
+                = GameCore.AxeManCore.GetComponent<SearchObject>()
+                .Search(SubTag.AimMarker)[0]
+                .GetComponent<MetaInfo>().Position;
+            int distance = GetComponent<LocalManager>().GetDistance(aimMarker);
+            int skillRange = GetSkillRange(
+                GetComponent<PCSkillManager>().GetSkillNameTag(e.UseSkill));
+
+            e.CanUseSkill.Push(distance <= skillRange);
+        }
+
         private void Start()
         {
+            GameCore.AxeManCore.GetComponent<AimMode>().VerifyingSkill
+                += SkillRange_VerifyingSkill;
+
             // TODO: Fill the dictionary by events.
             skillRangeDict[SkillNameTag.Q] = 3;
             skillRangeDict[SkillNameTag.W] = 4;
