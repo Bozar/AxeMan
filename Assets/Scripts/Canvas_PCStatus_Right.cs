5cff7ddf84834f5673cf657d4fe0c19f5f0b7028
bozar42 <5583771+Bozar@users.noreply.github.com>
2019-08-02 20:40:09 +0800

    Print enhance effects in PC status panel.

--- Assets/Scripts/Canvas_PCStatus_Right.cs
+++ Assets/Scripts/Canvas_PCStatus_Right.cs
@@ -4,6 +4,7 @@ using AxeMan.GameSystem.GameMode;
 using AxeMan.GameSystem.SearchGameObject;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,8 +13,10 @@ namespace AxeMan.GameSystem.UserInterface
     public class Canvas_PCStatus_Right : MonoBehaviour
     {
         private CanvasTag canvasTag;
+        private SkillTypeTag[] printEffect;
         private PCSkillManager skillManager;
         private SkillComponentTag[] sortedCurse;
+        private SkillComponentTag[] sortedEnhance;
         private UITag[] uiCurseData;
         private UITag[] uiCurseText;
         private GameObject[] uiObjects;
@@ -21,14 +24,22 @@ namespace AxeMan.GameSystem.UserInterface
         private void Awake()
         {
             canvasTag = CanvasTag.Canvas_PCStatus_Right;
+
             uiCurseText = new UITag[]
             {
                 UITag.Curse1Text, UITag.Curse2Text, UITag.Curse3Text,
             };
+
             uiCurseData = new UITag[]
             {
                 UITag.Curse1Data, UITag.Curse2Data, UITag.Curse3Data,
             };
+
+            printEffect = new SkillTypeTag[]
+            {
+                SkillTypeTag.Curse, SkillTypeTag.Enhance,
+            };
+
             sortedCurse = new SkillComponentTag[]
             {
                 SkillComponentTag.FireCurse,
@@ -36,9 +47,18 @@ namespace AxeMan.GameSystem.UserInterface
                 SkillComponentTag.AirCurse,
                 SkillComponentTag.EarthCurse,
             };
+
+            sortedEnhance = new SkillComponentTag[]
+            {
+                SkillComponentTag.FireMerit,
+                SkillComponentTag.WaterMerit,
+                SkillComponentTag.AirMerit,
+                SkillComponentTag.EarthMerit,
+            };
         }
 
-        private void Canvas_PCStatus_Right_CreatedWorld(object sender, EventArgs e)
+        private void Canvas_PCStatus_Right_CreatedWorld(object sender,
+            EventArgs e)
         {
             uiObjects = GetComponent<SearchUI>().Search(canvasTag);
             skillManager = GetComponent<SearchObject>().Search(SubTag.PC)[0]
@@ -55,10 +75,11 @@ namespace AxeMan.GameSystem.UserInterface
             Range(e.CommandTag);
             Cooldown(e.CommandTag);
             Damage(e.CommandTag);
-            Curse(e.CommandTag);
+            Effect(e.CommandTag);
         }
 
-        private void Canvas_PCStatus_Right_LeavingAimMode(object sender, EventArgs e)
+        private void Canvas_PCStatus_Right_LeavingAimMode(object sender,
+            EventArgs e)
         {
             ClearAllUIContent(uiObjects);
         }
@@ -78,34 +99,6 @@ namespace AxeMan.GameSystem.UserInterface
                 = skillManager.GetMaxCooldown(commandTag).ToString();
         }
 
-        private void Curse(CommandTag commandTag)
-        {
-            SkillTypeTag skillType = skillManager.GetSkillTypeTag(commandTag);
-            if (skillType != SkillTypeTag.Curse)
-            {
-                return;
-            }
-
-            SkillNameTag skillName = skillManager.GetSkillNameTag(commandTag);
-            Dictionary<SkillComponentTag, int[]> compInt
-                = skillManager.GetSkillEffect(skillName);
-            SkillComponentTag[] sortedComp = SortComponents(compInt);
-
-            string comp;
-            string effect;
-            SkillComponentTag current;
-
-            for (int i = 0; i < sortedComp.Length; i++)
-            {
-                current = sortedComp[i];
-                comp = skillManager.GetSkillComponentName(current);
-                effect = skillManager.GetSkillEffectName(current, compInt[current]);
-
-                SearchText(uiCurseText[i]).text = comp;
-                SearchText(uiCurseData[i]).text = effect;
-            }
-        }
-
         private void Damage(CommandTag commandTag)
         {
             SkillTypeTag skillType = skillManager.GetSkillTypeTag(commandTag);
@@ -117,6 +110,35 @@ namespace AxeMan.GameSystem.UserInterface
             SearchText(UITag.DamageText).text = "Dmg";
             SearchText(UITag.DamageData).text
                 = skillManager.GetSkillDamage(commandTag).ToString();
+        }
+
+        private void Effect(CommandTag commandTag)
+        {
+            SkillTypeTag skillType = skillManager.GetSkillTypeTag(commandTag);
+            if (!printEffect.Contains(skillType))
+            {
+                return;
+            }
+
+            SkillNameTag skillName = skillManager.GetSkillNameTag(commandTag);
+            Dictionary<SkillComponentTag, int[]> compInt
+                = skillManager.GetSkillEffect(skillName);
+            SkillComponentTag[] sortedComp = SortComponents(skillType, compInt);
+
+            string comp;
+            string effect;
+            SkillComponentTag current;
+
+            for (int i = 0; i < sortedComp.Length; i++)
+            {
+                current = sortedComp[i];
+                comp = skillManager.GetSkillComponentName(current);
+                effect = skillManager.GetSkillEffectName(current,
+                    compInt[current]);
+
+                SearchText(uiCurseText[i]).text = comp;
+                SearchText(uiCurseData[i]).text = effect;
+            }
         }
 
         private void Range(CommandTag commandTag)
@@ -133,12 +155,28 @@ namespace AxeMan.GameSystem.UserInterface
             return GetComponent<SearchUI>().SearchText(uiObjects, uiTag);
         }
 
-        private SkillComponentTag[] SortComponents(
+        private SkillComponentTag[] SortComponents(SkillTypeTag skillTypeTag,
             Dictionary<SkillComponentTag, int[]> compInt)
         {
             Queue<SkillComponentTag> sortedComp = new Queue<SkillComponentTag>();
+            SkillComponentTag[] checkList;
 
-            foreach (SkillComponentTag sct in sortedCurse)
+            switch (skillTypeTag)
+            {
+                case SkillTypeTag.Enhance:
+                    checkList = sortedEnhance;
+                    break;
+
+                case SkillTypeTag.Curse:
+                    checkList = sortedCurse;
+                    break;
+
+                default:
+                    checkList = new SkillComponentTag[] { };
+                    break;
+            }
+
+            foreach (SkillComponentTag sct in checkList)
             {
                 if (compInt.ContainsKey(sct))
                 {
