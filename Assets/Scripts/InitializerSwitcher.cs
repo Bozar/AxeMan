cdc4566e9b8778c47ba46a72f4a580faba076d8e
bozar42 <5583771+Bozar@users.noreply.github.com>
2019-08-24 08:40:07 +0800

    Disable initializer after the game is initialized.

---
+++ Assets/Scripts/InitializerSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace AxeMan.GameSystem.InitializeGameWorld
+{
+    public class InitializerSwitcher : MonoBehaviour
+    {
+        private void InitializerSwitcher_SwitchOffInitMainGame(object sender,
+            EventArgs e)
+        {
+            GetComponent<InitializeMainGame>().enabled = false;
+        }
+
+        private void Start()
+        {
+            GetComponent<InitializeMainGame>().SwitchOffInitMainGame
+                += InitializerSwitcher_SwitchOffInitMainGame;
+        }
+    }
+}
