c40d30a1f5331071b25fa7f4bb4c9726f9c5fe1f
bozar42 <5583771+Bozar@users.noreply.github.com>
2019-09-08 15:51:06 +0800

    Altar grants effect to PC.

--- Assets/Scripts/PCActiveAltar.cs
+++ Assets/Scripts/PCActiveAltar.cs
@@ -1,4 +1,5 @@
-﻿using AxeMan.GameSystem;
+﻿using AxeMan.DungeonObject.ActorSkill;
+using AxeMan.GameSystem;
 using AxeMan.GameSystem.GameDataTag;
 using AxeMan.GameSystem.SearchGameObject;
 using UnityEngine;
@@ -10,6 +11,7 @@ namespace AxeMan.DungeonObject
         private void PCActiveAltar_BlockingPCMovement(object sender,
             BlockPCMovementEventArgs e)
         {
+            // Check if PC bumps into an altar.
             int x = e.TargetPosition[0];
             int y = e.TargetPosition[1];
             if (!GameCore.AxeManCore.GetComponent<SearchObject>().Search(
@@ -18,6 +20,7 @@ namespace AxeMan.DungeonObject
                 return;
             }
 
+            // Check if altar is in cooldown.
             int current = GameCore.AxeManCore.GetComponent<AltarCooldown>()
                 .CurrentCooldown;
             int min = GameCore.AxeManCore.GetComponent<AltarCooldown>()
@@ -27,8 +30,24 @@ namespace AxeMan.DungeonObject
                 return;
             }
 
-            Debug.Log(altars[0].GetComponent<MetaInfo>().SubTag);
+            // Grant altar effect to PC.
+            SkillComponentTag skill
+                = GameCore.AxeManCore.GetComponent<AltarEffect>()
+                .GetEffect(altars[0].GetComponent<MetaInfo>().SubTag);
+            int powerDuration
+                = GameCore.AxeManCore.GetComponent<AltarEffect>()
+                .GetPowerDuration(altars[0].GetComponent<MetaInfo>().SubTag);
+            if (skill == SkillComponentTag.Life)
+            {
+                GetComponent<HP>().Add(powerDuration);
+            }
+            else
+            {
+                GetComponent<ActorStatus>().AddStatus(skill,
+                    new EffectData(powerDuration, powerDuration));
+            }
 
+            // End PC's turn.
             ActionTag action = ActionTag.ActiveAltar;
             GetComponent<LocalManager>().TakenAction(action);
             GetComponent<LocalManager>().CheckingSchedule(action);
