7bdfb1da0ea1c1a254ea74b0c80d6c98d7a4e720
bozar42 <5583771+Bozar@users.noreply.github.com>
2019-09-17 20:32:02 +0800

    Save altar positions in UpgradeAltar.cs.

--- Assets/Scripts/UpgradeAltar.cs
+++ Assets/Scripts/UpgradeAltar.cs
@@ -2,18 +2,24 @@
 using AxeMan.GameSystem.GameDataTag;
 using AxeMan.GameSystem.GameEvent;
 using AxeMan.GameSystem.InitializeGameWorld;
+using AxeMan.GameSystem.SearchGameObject;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AxeMan.GameSystem
 {
     public class UpgradeAltar : MonoBehaviour
     {
+        private int[][] altarPositions;
         private GameObject pc;
 
         private void Start()
         {
             GetComponent<InitializeMainGame>().SettingReference
                 += UpgradeAltar_SettingReference;
+            GetComponent<InitializeMainGame>().CreatedWorld
+                += UpgradeAltar_CreatedWorld;
             GetComponent<PublishHP>().ChangedHP += UpgradeAltar_ChangedHP;
         }
 
@@ -26,6 +32,24 @@ namespace AxeMan.GameSystem
 
             int[] pos = pc.GetComponent<MetaInfo>().Position;
             Debug.Log(pos[0] + "," + pos[1]);
+            Debug.Log("==========");
+            foreach (int[] p in altarPositions)
+            {
+                Debug.Log(p[0] + "," + p[1]);
+            }
+        }
+
+        private void UpgradeAltar_CreatedWorld(object sender, EventArgs e)
+        {
+            GameObject[] altars = GetComponent<SearchObject>().Search(
+                MainTag.Altar);
+            Stack<int[]> position = new Stack<int[]>();
+
+            foreach (GameObject go in altars)
+            {
+                position.Push(go.GetComponent<MetaInfo>().Position);
+            }
+            altarPositions = position.ToArray();
         }
 
         private void UpgradeAltar_SettingReference(object sender,
