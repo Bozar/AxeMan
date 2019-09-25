0f9626eb64b97f8dc9345b32fa677930f26c4a76
bozar42 <5583771+Bozar@users.noreply.github.com>
2019-09-25 20:31:25 +0800

    Add a private switcher to SearchObject.Search(int x, int y).

--- Assets/Scripts/SearchObject.cs
+++ Assets/Scripts/SearchObject.cs
@@ -71,6 +71,8 @@ namespace AxeMan.GameSystem.SearchGameObject
 
     public class SearchObject : MonoBehaviour, ISearchObject
     {
+        private bool searchInArray;
+
         public event EventHandler<SearchingIDEventArgs> SearchingID;
 
         public event EventHandler<SearchingMainTagEventArgs> SearchingMainTag;
@@ -99,12 +101,19 @@ namespace AxeMan.GameSystem.SearchGameObject
 
         public GameObject[] Search(int x, int y)
         {
-            int[] pos = new int[] { x, y };
-            Stack<GameObject> data = new Stack<GameObject>();
-            var ea = new SearchingPositionEventArgs(pos, data);
+            if (searchInArray)
+            {
+                return null;
+            }
+            else
+            {
+                int[] pos = new int[] { x, y };
+                Stack<GameObject> data = new Stack<GameObject>();
+                var ea = new SearchingPositionEventArgs(pos, data);
 
-            OnSearchingPosition(ea);
-            return ea.Data.ToArray();
+                OnSearchingPosition(ea);
+                return ea.Data.ToArray();
+            }
         }
 
         public GameObject[] Search(MainTag mainTag)
@@ -158,6 +167,11 @@ namespace AxeMan.GameSystem.SearchGameObject
         protected virtual void OnSearchingSubTag(SearchingSubTagEventArgs e)
         {
             SearchingSubTag?.Invoke(this, e);
+        }
+
+        private void Awake()
+        {
+            searchInArray = false;
         }
 
         private GameObject[] Filter(GameObject[] source, MainTag mainTag)
