be82e7a1ecd21df8a57b90ff018eea9d8eacd93c
bozar42 <5583771+Bozar@users.noreply.github.com>
2019-11-16 12:44:35 +0800

    Publish events when entering or leaving log mode.

--- Assets/Scripts/LogMode.cs
+++ Assets/Scripts/LogMode.cs
@@ -1,18 +1,47 @@
 ﻿using AxeMan.GameSystem.GameDataTag;
 using AxeMan.GameSystem.PlayerInput;
+using System;
 using UnityEngine;
 
 namespace AxeMan.GameSystem.GameMode
 {
     public class LogMode : MonoBehaviour
     {
+        public event EventHandler<EventArgs> EnteringLogMode;
+
+        public event EventHandler<EventArgs> LeavingLogMode;
+
+        protected virtual void OnEnteringLogMode(EventArgs e)
+        {
+            EnteringLogMode?.Invoke(this, e);
+        }
+
+        protected virtual void OnLeavingLogMode(EventArgs e)
+        {
+            LeavingLogMode?.Invoke(this, e);
+        }
+
+        private bool EnterLogMode(PlayerCommandingEventArgs e)
+        {
+            return (e.SubTag == SubTag.PC) && (e.Command == CommandTag.LogMode);
+        }
+
+        private bool LeaveLogMode(PlayerCommandingEventArgs e)
+        {
+            return (e.SubTag == SubTag.LogMarker)
+                && (e.Command == CommandTag.Cancel);
+        }
+
         private void LogMode_PlayerCommanding(object sender,
             PlayerCommandingEventArgs e)
         {
-            if ((e.SubTag == SubTag.PC)
-                && (e.Command == CommandTag.LogMode))
+            if (EnterLogMode(e))
             {
-                Debug.Log("Log mode");
+                OnEnteringLogMode(EventArgs.Empty);
+            }
+            else if (LeaveLogMode(e))
+            {
+                OnLeavingLogMode(EventArgs.Empty);
             }
         }
 
