84e99810931ab2c0bc26f806cea476e7b0119204
bozar42 <5583771+Bozar@users.noreply.github.com>
2019-11-18 21:34:55 +0800

    Print game mode in help panel.

--- Assets/Scripts/Canvas_Help.cs
+++ Assets/Scripts/Canvas_Help.cs
@@ -1,4 +1,5 @@
-﻿using AxeMan.GameSystem.GameDataTag;
+﻿using AxeMan.GameSystem.GameDataHub;
+using AxeMan.GameSystem.GameDataTag;
 using AxeMan.GameSystem.GameMode;
 using AxeMan.GameSystem.InitializeGameWorld;
 using AxeMan.GameSystem.SearchGameObject;
@@ -21,8 +22,20 @@ namespace AxeMan.GameSystem.UserInterface
         private void Canvas_Help_CreatedWorld(object sender, EventArgs e)
         {
             uiObjects = GetComponent<SearchUI>().Search(canvasTag);
+            PrintNormalModeText();
+        }
 
-            SearchText(UITag.UIText).text = "Help menu";
+        private void Canvas_Help_EnteringAimMode(object sender,
+            EnterAimModeEventArgs e)
+        {
+            ClearText();
+            PrintAimModeText();
+        }
+
+        private void Canvas_Help_EnteringExamineMode(object sender, EventArgs e)
+        {
+            ClearText();
+            PrintExamineModeText();
         }
 
         private void Canvas_Help_EnteringLogMode(object sender, EventArgs e)
@@ -30,9 +43,56 @@ namespace AxeMan.GameSystem.UserInterface
             SwitchVisibility(false);
         }
 
+        private void Canvas_Help_LeavingAimMode(object sender, EventArgs e)
+        {
+            ClearText();
+            PrintNormalModeText();
+        }
+
+        private void Canvas_Help_LeavingExamineMode(object sender, EventArgs e)
+        {
+            ClearText();
+            PrintNormalModeText();
+        }
+
         private void Canvas_Help_LeavingLogMode(object sender, EventArgs e)
         {
             SwitchVisibility(true);
+        }
+
+        private void ClearText()
+        {
+            foreach (GameObject go in uiObjects)
+            {
+                go.GetComponent<Text>().text = "";
+            }
+        }
+
+        private string GetStringData(UITextDataTag uiTextDataTag)
+        {
+            string text = GetComponent<UITextData>().GetStringData(
+                UITextCategoryTag.Help, uiTextDataTag);
+            GetComponent<ColorManager>().SetColor(text, ColorTag.Grey, out text);
+
+            return text;
+        }
+
+        private void PrintAimModeText()
+        {
+            SearchText(UITag.Modeline).text = GetStringData(
+                UITextDataTag.AimMode);
+        }
+
+        private void PrintExamineModeText()
+        {
+            SearchText(UITag.Modeline).text = GetStringData(
+                UITextDataTag.ExamineMode);
+        }
+
+        private void PrintNormalModeText()
+        {
+            SearchText(UITag.Modeline).text = GetStringData(
+                UITextDataTag.NormalMode);
         }
 
         private Text SearchText(UITag uiTag)
@@ -49,6 +109,16 @@ namespace AxeMan.GameSystem.UserInterface
                 += Canvas_Help_EnteringLogMode;
             GetComponent<LogMode>().LeavingLogMode
                 += Canvas_Help_LeavingLogMode;
+
+            GetComponent<AimMode>().EnteringAimMode
+                += Canvas_Help_EnteringAimMode;
+            GetComponent<AimMode>().LeavingAimMode
+                += Canvas_Help_LeavingAimMode;
+
+            GetComponent<ExamineMode>().EnteringExamineMode
+                += Canvas_Help_EnteringExamineMode;
+            GetComponent<ExamineMode>().LeavingExamineMode
+                += Canvas_Help_LeavingExamineMode;
         }
 
         private void SwitchVisibility(bool switchOn)
