fba2200245dc9a140d9007614536a0dd2bc26750
bozar42 <5583771+Bozar@users.noreply.github.com>
2019-12-27 21:57:45 +0800

    Remove unused code.

--- Assets/Scripts/Canvas_BuildSkill.cs
+++ Assets/Scripts/Canvas_BuildSkill.cs
@@ -1,17 +1,14 @@
 ﻿using AxeMan.GameSystem.GameDataTag;
 using AxeMan.GameSystem.GameMode;
 using AxeMan.GameSystem.InitializeGameWorld;
-using AxeMan.GameSystem.SearchGameObject;
 using System;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace AxeMan.GameSystem.UserInterface
 {
     public class Canvas_BuildSkill : MonoBehaviour
     {
         private CanvasTag canvasTag;
-        private GameObject[] uiObjects;
 
         private void Awake()
         {
@@ -21,11 +18,7 @@ namespace AxeMan.GameSystem.UserInterface
         private void Canvas_BuildSkill_LoadingGameData(object sender,
             EventArgs e)
         {
-            uiObjects = GetComponent<SearchUI>().Search(canvasTag);
             SwitchVisibility(false);
-
-            // TODO: Remove this later.
-            SearchText(UITag.UIText).GetComponent<Text>().text = "Build skill";
         }
 
         private void Canvas_BuildSkill_SwitchingGameMode(object sender,
@@ -39,11 +32,6 @@ namespace AxeMan.GameSystem.UserInterface
             {
                 SwitchVisibility(true);
             }
-        }
-
-        private Text SearchText(UITag uiTag)
-        {
-            return GetComponent<SearchUI>().SearchText(uiObjects, uiTag);
         }
 
         private void Start()
